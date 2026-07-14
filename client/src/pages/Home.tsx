b986fb248b07397cbdd0600df785ca077c00c602
Hygor Lorençato <hygorlc92@gmail.com>
2026-07-14 12:37:10 -0300

    Capa nova da categoria Anéis

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -15,7 +15,7 @@ const CATEGORIES = [
   {
     id: "aneis",
     label: "Anéis",
-    img: "https://d2xsxph8kpxj0f.cloudfront.net/310519663787846659/aeFGMeyncKnfvU6NJgH559/category_aneis-g6qDG8EcPAudu7dNYXPBZ7.webp",
+    img: "/images/category_aneis.jpg",
   },
   {
     id: "correntes",
